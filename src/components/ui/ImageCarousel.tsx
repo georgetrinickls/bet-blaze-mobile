9bc3f8b57f9cdf4fb968eb7b5999db50d2be5ac6
georgetrinickls <george.trinick@livescore.com>
2025-05-12 20:29:25 +0100

    Create ImageCarousel.tsx

---
+++ src/components/ui/ImageCarousel.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+const ImageCarousel: React.FC = () => {
+  const images = [
+    "https://cdn.virginbet.com/headlines/images/vb-3kroulette-1105.jpg",
+    "https://cdn.virginbet.com/headlines/images/vb-newweeklyaccaoffer.jpg",
+    "https://cdn.virginbet.com/headlines/images/vb-2up.png",
+    "https://www.virginbet.com/about-us/VBUK-Gates-Of-Hades-Non-Promo-Banner-55685.jpg",
+    "https://www.virginbet.com/about-us/VBUK-Generic-Banner-Triple-Phoenix-Power-Combo-67516.jpg",
+    "https://www.virginbet.com/about-us/VB-4132-Network-Promotion-Games-Global-Race-2-Riches-21APR-29JUN-60027.jpg",
+    "https://www.virginbet.com/about-us/dfg-55573.jpg",
+    "https://www.virginbet.com/about-us/VBUK-4172-Drop-and-Wins-Slots-2025-Stage-3-30Apr-04June-45899.jpg",
+    "https://www.virginbet.com/about-us/vbcrazytimeGENERIC-71907.jpg",
+    "https://www.virginbet.com/about-us/VBUK-Generic-Virgin-Bet-Pro-Blackjack-63521.jpg",
+    "https://www.virginbet.com/about-us/Generic-Crazy-Balls-91363.jpg",
+    "https://www.virginbet.com/about-us/Generic-Game-Shows-84548.jpg",
+    "https://www.virginbet.com/about-us/vblightningrouletteGENERIC-57511.jpg"
+  ];
+
+  return (
+    <div className="w-full flex justify-center pt-3">
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={20}
+        slidesPerView={1.2}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 10000,
+          disableOnInteraction: false,
+        }}
+        className="overflow-visible"
+        style={{ width: '384px', height: '133px' }}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide
+            key={index}
+            style={{ width: '384px', height: '133px' }}
+            className="rounded-lg overflow-hidden"
+          >
+            <img 
+              src={image} 
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageCarousel;
